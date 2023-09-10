6c9f03cdc4851490fe8db14aff37ad4849fa948c
Ryan-Gillooly-CKO <ryangillooly@hotmail.co.uk>
2023-09-10 21:34:40 +0100

    remove all

--- LoyaltySystem.Core/Models/Location.cs
+++
@@ -1,8 +0,0 @@
-namespace LoyaltySystem.Core.Models;
-
-public class Location
-{
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public string Address { get; set; }
-}
