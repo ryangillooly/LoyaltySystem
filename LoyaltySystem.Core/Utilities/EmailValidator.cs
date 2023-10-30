75d05b6160ae072d4d350d6c3da86b74bbe249c2
ryangillooly <ryangillooly@hotmail.co.uk>
2023-10-30 21:36:06 +0000

    +tests

--- LoyaltySystem.Core/Utilities/EmailValidator.cs
+++ LoyaltySystem.Core/Utilities/EmailValidator.cs
@@ -4,7 +4,10 @@ namespace LoyaltySystem.Core.Utilities;
 
 public static class EmailValidator
 {
-    private static readonly Regex EmailPattern = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+    private static readonly Regex EmailPattern = new Regex(
+        @"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+        RegexOptions.Compiled | RegexOptions.IgnoreCase
+    );
 
     public static bool IsValidEmail(this string email) => EmailPattern.IsMatch(email);
 }
